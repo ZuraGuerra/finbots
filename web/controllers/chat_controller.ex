57088afc8dfc7585db10989a8463084b4d6cb787
Zura Guerra <guerrazuramx@gmail.com>
2016-10-30 08:15:36 -0600

    Send button template on bot activation

--- web/controllers/chat_controller.ex
+++ web/controllers/chat_controller.ex
@@ -7,6 +7,45 @@ defmodule Cocodrilo.ChatController do
   def chat(conn, %{"hub.challenge" => challenge}),
     do: render conn, "challenge.json", challenge: challenge
 
+  def chat(conn, %{"entry" => [%{"messaging" => [%{"postback" => %{"payload" => "Chatea con un asesor"},
+                                                   "recipient" => %{"id" => page_id},
+                                                   "sender" => %{"id" => user_id}}|_]}|_]}) do
+   IO.puts "YA entré"
+    message = %{
+      "recipient" => %{"id" => user_id},
+      "message" => %{
+        "attachment" => %{
+          "type" => "template",
+          "payload" => %{
+            "template_type" => "button",
+            "text" => "Hola, soy tu asistente personal de Doge Bank. ¿Cómo puedo ayudarte?",
+            "buttons" => [
+              %{
+                "type" => "postback",
+                "title" => "Reportar mi tarjeta",
+                "payload" => "Quiero reportar mi tarjeta"
+              },
+              %{
+                "type" => "postback",
+                "title" => "Ver mi saldo",
+                "payload" => "Me gustaría conocer mi saldo"
+              },
+              %{
+                "type" => "postback",
+                "title" => "Conversión de divisas",
+                "payload" => "Quiero saber su precio de cambio para una divisa extranjera"
+              }
+            ]
+          }
+        }
+      }
+    }
+    message = message |> JSX.encode!
+    url = @messages_url <> @page_token
+    System.cmd("curl", ["-X", "POST", "-H", "Content-Type: application/json", "-d", message, url])
+    render conn, "test.json", text: "text"
+  end
+
   def chat(conn, %{"entry" => [%{"messaging" => [%{"message" => %{"text" => text}}|_]}|_]}) do
     message = %{
       "recipient" => %{"id" => 1104490799647266},
