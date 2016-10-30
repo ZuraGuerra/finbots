9117cb1a1d2b824596ba32c8b1af2ffc45499b00
Zura Guerra <guerrazuramx@gmail.com>
2016-10-30 02:17:20 -0600

    WIP // Send buttons to client

--- web/controllers/chat_controller.ex
+++ web/controllers/chat_controller.ex
@@ -1,12 +1,44 @@
 defmodule Cocodrilo.ChatController do
   use Cocodrilo.Web, :controller
-  require IEx
+
+  @messages_url "https://graph.facebook.com/v2.6/me/messages?access_token="
+  @page_token System.get_env("PAGE_TOKEN")
 
   def chat(conn, %{"hub.challenge" => challenge}),
     do: render conn, "challenge.json", challenge: challenge
 
   def chat(conn, %{"entry" => [%{"messaging" => [%{"message" => %{"text" => text}}|_]}|_]}) do
-    IO.puts text
+    message = %{
+      "recipient" => %{"id" => 1104490799647266},
+      "message" => %{
+        "attachment" => %{
+          "type" => "template",
+          "payload" => %{
+            "template_type" => "button",
+            "text" => "uwu?",
+            "buttons" => [
+              %{
+                "type" => "web_url",
+                "url" => "http://zura.space",
+                "title" => "ZURINHA"
+              },
+              %{
+                "type" => "postback",
+                "title" => "OvO",
+                "payload" => "KHÉ?!"
+              }
+            ]
+          }
+        }
+      }
+    }
+    message = message |> JSX.encode!
+    url = @messages_url <> @page_token
+    System.cmd("curl", ["-X", "POST", "-H", "Content-Type: application/json", "-d", message, url])
     render conn, "test.json", text: text
+  end
+
+  def chat(conn, %{"object" => "page"}) do
+    render conn, "callback_response.json"
   end
 end
--- web/views/chat_view.ex
+++ web/views/chat_view.ex
@@ -4,4 +4,6 @@ defmodule Cocodrilo.ChatView do
   def render("challenge.json", %{challenge: challenge}), do: challenge |> String.to_integer
 
   def render("test.json", %{text: text}), do: %{}
+
+  def render("callback_response.json", %{}), do: %{}
 end
